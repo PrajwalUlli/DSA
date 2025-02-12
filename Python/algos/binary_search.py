23331a2c9e56dea11a0ca5d78d03a8f930976aa9
PrajwalUlli <ulliprajwaledu@gmail.com>
2025-02-12 21:41:12 +0530

    added psuedocode to selection sort

--- Python/algos/binary_search.py
+++ Python/algos/binary_search.py
@@ -16,9 +16,9 @@ def binary_search(arr: list, target: int or str or float) -> int:
 """
 1. Label the low and high points in the list
 2. Iterate over the list given that both the points dont collide:
-      a. with every iteration get the mid point in the list
-      b. if the mid == target: return the index
-      c. elif mid > target: eliminate the right half of the list ie update "high" label
-      d. else: eliminate the left half of the list ie update "low" label
+    a. with every iteration get the mid point in the list
+    b. if the mid == target: return the index
+    c. elif mid > target: eliminate the right half of the list ie update "high" label
+    d. else: eliminate the left half of the list ie update "low" label
 3. Return -1 if target not found.
 """
--- Python/algos/selection_sort.py
+++ Python/algos/selection_sort.py
@@ -1,22 +1,32 @@
 def selection_sort(arr: list) -> list:
-  # empty list check
-  if len(arr) == 0:
-    return []
-  
-  # arrCopy = list(arr)
-  arrCopy = arr.copy()
-  sortedArr = []
-  for i in range(0, len(arrCopy)):
-    min_index = find_smallest_idx(arrCopy)
-    min = arrCopy.pop(min_index)
-    sortedArr.append(min)
-  return sortedArr
+    # empty list check
+    if len(arr) == 0:
+        return arr
+
+    copyArr = arr.copy()
+    sortedArr = []
+    for i in range(0, len(copyArr)):
+        min_index = find_smallest_idx(copyArr)
+        min = copyArr.pop(min_index)
+        sortedArr.append(min)
+    return sortedArr
 
 def find_smallest_idx(arr: list) -> int:
-  min_idx = 0
-  for i in range(0, len(arr)):
-    if arr[i] < arr[min_idx]:
-      min_idx = i
-    else:
-      continue
-  return min_idx
+    min_idx = 0
+    for i in range(0, len(arr)):
+        if arr[i] < arr[min_idx]:
+            min_idx = i
+        else:
+            continue
+    return min_idx
+
+"""
+1. If the list given is empty: return the list
+2. Create a copy of the original list
+3. Declare a new empty list "sortedArr"
+4. Start a loop of the lenght of copied list:
+    a. get the smallest value from the copied list
+    b. remove it from the copied list
+    c. add that smallest value to "sortedArr" list
+5. Return "sortedArr"
+"""
